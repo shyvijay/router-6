8935fe0cd8b02050a17a4c12ebd12964a3dfae36
Vijay Swami <vijay.swami@autodesk.com>
2023-02-23 20:04:44 +0530

    navbar styling is done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,42 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 export const Header = () => {
-  const getClass = ({ isActive }) => (isActive ? "active" : "");
+  // const getClass = ({ isActive }) => (isActive ? "active" : "");
   return (
-    <nav>
-      <NavLink to="/" className={getClass}>
-        Home
-      </NavLink>
-      <NavLink to="/about" className={getClass}>
-        About
-      </NavLink>
-      <NavLink to="/categories" className={getClass}>
-        Categories
-      </NavLink>
-      <NavLink to="/register" className={getClass}>
-        Register
-      </NavLink>
-    </nav>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand>
+          <NavLink to="/">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png"
+              alt="logo"
+              style={{ width: 50, display: "inline-block" }}
+            />
+          </NavLink>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+            <NavLink to="/about" className="nav-link">
+              About
+            </NavLink>
+            <NavLink to="/categories" className="nav-link">
+              Categories
+            </NavLink>
+            <NavLink to="/register" className="nav-link">
+              Register
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
